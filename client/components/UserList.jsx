42e7b48d58973e9b34c09223cae4eaa0cb67f97e
lgrees <lukasgrees@gmail.com>
2018-10-16 14:24:58 -0500

    adds random message

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import User from './User.jsx';
-
+console.log('hello-world-lol');
 export default ({ users }) => (
   <div className="user-list">
     <p className="lead">
-      Here's a list of {users.length} completely fictitious people:
+      Here's a list of {users.length} completely fictitious people with some new
+      text here:
     </p>
 
     <table className="table">
